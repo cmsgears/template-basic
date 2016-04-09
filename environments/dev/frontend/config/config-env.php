e87a9136ad7609db758122cf35e6a8c99820f576
Bhagwat Chouhan <bhagwat.chouhan@vulpinecode.com>
2016-04-09 14:46:44 +0530

    Updated environments.

--- environments/dev/frontend/config/config-env.php
+++ environments/dev/frontend/config/config-env.php
@@ -8,14 +8,13 @@ $config = [
     ]
 ];
 
-// Debugger and Gii extensions required for dev environment
 if( !YII_ENV_TEST ) {
 
     $config['bootstrap'][] 		= 'debug';
-    $config['modules']['debug'] = 'yii\debug\Module';
+    $config['modules']['debug'] = [ 'class' => 'yii\debug\Module' ];
 
     $config['bootstrap'][] 		= 'gii';
-    $config['modules']['gii'] 	= 'yii\gii\Module';
+    $config['modules']['gii'] 	= [ 'class' => 'yii\gii\Module' ];
 }
 
 return $config;
---
+++ environments/dev/tests/codeception/config/config-env.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'components' => [
+        'db' => [
+            'class' => 'yii\db\Connection',
+            'dsn' => 'mysql:host=localhost;dbname=nutriquest',
+            'username' => 'nutriquest',
+            'password' => 'nutri#quest',
+            'charset' => 'utf8'
+        ]
+    ]
+];
+
+?>

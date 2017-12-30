38b68727c13bfb64ff9c34c1045b28adf0fed94c
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2017-12-30 13:54:24 +0530

    Applied quick fixes to migrations.

--- console/migrations/data/m160623_072922_forms.php
+++ console/migrations/data/m160623_072922_forms.php
@@ -16,7 +16,7 @@ class m160623_072922_forms extends \yii\db\Migration {
 	// Private Variables
 
 	private $cmgPrefix;
-	private $appPrefix;
+	private $sitePrefix;
 
 	private $site;
 
@@ -26,7 +26,7 @@ class m160623_072922_forms extends \yii\db\Migration {
 
 		// Table prefix
 		$this->cmgPrefix	= Yii::$app->migration->cmgPrefix;
-		$this->appPrefix	= Yii::$app->migration->appPrefix;
+		$this->sitePrefix	= Yii::$app->migration->sitePrefix;
 
 		$this->site		= Site::findBySlug( CoreGlobal::SITE_MAIN );
 		$this->master	= User::findByUsername( Yii::$app->migration->getSiteMaster() );

7c166f014e9ee45d9a18a06e9852e498962986f2
Bhagwat Singh Chouhan <bhagwat.chouhan@vulpinecode.com>
2018-01-31 07:16:16 +0530

    Added templates structure.

--- common/config/bootstrap.php
+++ common/config/bootstrap.php
@@ -3,6 +3,7 @@
 Yii::setAlias( '@common', dirname( __DIR__ ) );
 Yii::setAlias( '@themes', dirname( dirname( __DIR__ ) ) . '/themes' );
 Yii::setAlias( '@widgets', dirname( dirname( __DIR__ ) ) . '/widgets' );
+Yii::setAlias( '@templates', dirname( dirname( __DIR__ ) ) . '/templates' );
 Yii::setAlias( '@uploads', dirname( dirname( __DIR__ ) ) . '/uploads' );
 
 // Applications
